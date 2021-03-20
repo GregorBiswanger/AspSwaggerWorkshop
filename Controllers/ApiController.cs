458df3cd4283798d44062a22d21bbaead668ab51
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2021-03-21 00:25:20 +0100

    First commit

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using AspRestApiWorkshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace AspRestApiWorkshop.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly LinkGenerator _linkGenerator;
+
+        public ApiController(LinkGenerator linkGenerator)
+        {
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public ActionResult<ApiModel[]> Get()
+        {
+            return new ApiModel[]
+            {
+                new ApiModel
+                {
+                    Name = "Camps",
+                    Description = "Dev Conferences",
+                    Links = new List<LinkDto>
+                    {
+                        new LinkDto(_linkGenerator.GetUriByAction(HttpContext, "GetCamps", "camps"),
+                                    "get_camps", 
+                                    "GET")
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,49 @@
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace AspRestApiWorkshop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CampContext>();
+            services.AddScoped<ICampRepository, CampRepository>();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddControllers()
+                .AddXmlDataContractSerializerFormatters();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
